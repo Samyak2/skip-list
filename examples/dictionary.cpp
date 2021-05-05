0f872c00b1ea62f34174ff8d1d7498d8787b1a2a
Samyak S Sarnayak <samyak201@gmail.com>
2021-05-05 18:18:38 +0530

    simplified dictionary example

--- examples/dictionary.cpp
+++ examples/dictionary.cpp
@@ -12,19 +12,11 @@ public:
   DictKey(int key) : key_(key), value_("") {}
   DictKey(int key, std::string value) : key_(key), value_(value) {}
   friend bool operator<(const DictKey &, const DictKey &);
-  friend bool operator==(const DictKey &, const DictKey &);
-  friend bool operator!=(const DictKey &, const DictKey &);
   friend std::ostream &operator<<(std::ostream &out, const DictKey &o);
 };
 
 bool operator<(const DictKey &left, const DictKey &right) {
   return left.key_ < right.key_;
-}
-bool operator==(const DictKey &left, const DictKey &right) {
-  return left.key_ == right.key_;
-}
-bool operator!=(const DictKey &left, const DictKey &right) {
-  return left.key_ != right.key_;
 }
 std::ostream &operator<<(std::ostream &out, const DictKey &o) {
   return out << "<" << o.key_ << ": " << o.value_ << ">";
@@ -42,10 +34,10 @@ void foundit(skiplist<DictKey> &s, DictKey x) {
 int main() {
   skiplist<DictKey> aDict;
 
-  aDict.insert(DictKey{10, "hello"});
-  aDict.insert(DictKey{20, "jello"});
-  aDict.insert(DictKey{20, "yello"});
-  aDict.insert(DictKey{40, "fello"});
+  aDict.insert({10, "hello"});
+  aDict.insert({20, "jello"});
+  aDict.insert({20, "yello"});
+  aDict.insert({40, "fello"});
 
   // visualize(aDict);
 
@@ -58,10 +50,10 @@ int main() {
 
   // same operations on multiset
   std::multiset<DictKey> lolz;
-  lolz.insert(DictKey{10, "hello"});
-  lolz.insert(DictKey{20, "jello"});
-  lolz.insert(DictKey{20, "yello"});
-  lolz.insert(DictKey{40, "fello"});
+  lolz.insert({10, "hello"});
+  lolz.insert({20, "jello"});
+  lolz.insert({20, "yello"});
+  lolz.insert({40, "fello"});
 
   std::cout << "MULTISET:\n";
   for(auto e: lolz)
@@ -70,17 +62,17 @@ int main() {
   // what about a find?
   // returns the first element inserted apparently
   std::cout << "MULTISET:\n";
-  auto it = lolz.find(DictKey{20, "yomama"});
+  auto it = lolz.find(20);
   std::cout << "Found: " << *it << "\n";
   std::cout << "Next: " << *++it << "\n";
-  std::cout << "Count: " << lolz.count(DictKey{20, "yomama"}) << "\n";
+  std::cout << "Count: " << lolz.count(20) << "\n";
 
   // now to repeat it with our data structure
   std::cout << "SKIPLIST:\n";
-  auto iter = aDict.find(DictKey{20, "yomama"});
+  auto iter = aDict.find(20);
   std::cout << "Found: " << *iter << "\n";
   std::cout << "Next: " << *++iter << "\n";
-  std::cout << "Count: " << aDict.count(DictKey{20, "yomama"}) << "\n";
+  std::cout << "Count: " << aDict.count(20) << "\n";
 
   return 0;
 }
